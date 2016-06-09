deaba9c3bf8c51ccc65c3dc575c962f891904609
Dmitry Pashinskiy <pashinskiy.kh.cr@gmail.com>
2016-06-09 19:00:28 +0300

    clean up

--- Example/Pods/Target Support Files/CRNetworkButton/CRNetworkButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CRNetworkButton : NSObject
-@end
-@implementation PodsDummy_CRNetworkButton
-@end
--- Example/Pods/Target Support Files/CRNetworkButton/CRNetworkButton-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double CRNetworkButtonVersionNumber;
-FOUNDATION_EXPORT const unsigned char CRNetworkButtonVersionString[];
-
